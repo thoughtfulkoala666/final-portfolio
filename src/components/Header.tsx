2c8c4dee1fbf92606b1ac5f8603eca2eb60315b5
David Fryd <114892840+thoughtfulkoala666@users.noreply.github.com>
2022-12-12 05:48:24 -0500

    title links to home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
+import { Link } from "react-router-dom";
 export default function Header() {
     return (
         <div
             id="very-top"
             className="h-30 border-b-2 border-t-2 w-full font-bold text-2xl font-mono flex flex-col items-center justify-center"
         >
-            <p className="font-bold text-4xl xl:text-6xl pt-6 pb-2 text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-pink-600">
+            <a
+                href={"/"}
+                className="font-bold text-4xl xl:text-6xl pt-6 pb-2 text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-pink-600"
+            >
                 thoughtfulkoala666
-            </p>
+            </a>
             {/* <div className="p-2" /> */}
             <div
                 id="nav-items"
